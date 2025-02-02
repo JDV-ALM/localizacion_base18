fa6b22f114788fc59b5a506415218a8fe4f6a4bc
dsojo1980 <dsojo.tanfe@gmail.com>
2025-02-02 17:32:10 -0400

    modulos localizacion contable v18

---
+++ localizacion_18/iva_retention/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+{
+        'name': 'Retenciones de IVA para Venezuela v18',
+        'version': '0.1',
+        'author': 'Ing. Darrell Sojo / Alianza Frank Service',
+        'summary': 'Retenciones de IVA para Venezuela v18',
+        'description': """Retenciones de IVA para Venezuela v18.""",
+        'category': 'Accounting/Accounting',
+        'website': '',
+        'images': [],
+        'depends': [
+            'account',
+            'account_accountant',
+            'base',
+            'base_contable',
+            ],
+        'data': [
+            'security/ir.model.access.csv',
+            'views/retention_vat_provee_views.xml',
+            'views/retention_vat_cliente_views.xml',
+            'views/partner_views.xml',
+            'views/res_company_inherit.xml',
+            'views/account_move_views.xml',
+            'report/vat_wh_voucher.xml',            
+            ],
+        'installable': True,
+        'application': True,
+        'auto_install': False,
+                      
+}
